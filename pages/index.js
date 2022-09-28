e57e2ef2edd5b2cfafe4962415a6a24a07ef7301
PiotrekNiemirski <piotr.niemirski@they.pl>
2022-09-28 18:02:17 +0200

    fix: reset project

--- pages/index.js
+++ pages/index.js
@@ -1,57 +1,9 @@
-// import path from "path";
-// import fs from "fs/promises";
-
-// import Link from "next/link";
-import Head from "next/head";
-
-import { getFeaturedEvents } from "../helpers/api-util";
-import EventList from "../components/events/events-list";
-
-function HomePage(props) {
-  const { events } = props;
-
+function HomePage() {
   return (
-    <>
-      <Head>
-        <title>NextJS Events</title>
-        <meta name="description" content="Find a lot of great events!" />
-      </Head>
-      <EventList items={events} />
-      {/* <ul>
-        {products.map((product) => (
-          <li key={product.id}>
-            <Link href={`/products/${product.id}`}>{product.title}</Link>
-          </li>
-        ))}
-      </ul> */}
-    </>
+    <div>
+      <h1>The Home Page</h1>
+    </div>
   );
-}
-
-export async function getStaticProps() {
-  // const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
-  // const jsonData = await fs.readFile(filePath);
-  // const data = JSON.parse(jsonData);
-  const featuredEevents = await getFeaturedEvents();
-
-  if (!featuredEevents) {
-    return {
-      redirect: {
-        destination: "/no-data",
-      },
-    };
-  }
-
-  if (featuredEevents.length === 0) {
-    return { notFound: true };
-  }
-
-  return {
-    props: {
-      events: featuredEevents,
-    },
-    revalidate: 1800,
-  };
 }
 
 export default HomePage;
