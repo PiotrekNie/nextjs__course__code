ee3d4af7413282c9dc9ee42bb9e04bd93e1c508f
PiotrekNiemirski <piotr.niemirski@they.pl>
2022-09-29 21:58:58 +0200

    feat: finish module

---
+++ pages/api/feedback/index.js
@@ -0,0 +1,41 @@
+import fs from "fs";
+import path from "path";
+
+export function buldFeedbackPath() {
+  return path.join(process.cwd(), "data", "feedback.json");
+}
+
+export function extractFeedback(filePath) {
+  const fileData = fs.readFileSync(filePath);
+
+  return JSON.parse(fileData);
+}
+
+function handler(req, res) {
+  if (req.method === "POST") {
+    const email = req.body.email;
+    const feedback = req.body.feedback;
+
+    const newFeedback = {
+      id: new Date().toISOString(),
+      email: email,
+      text: feedback,
+    };
+
+    // store that in a database or in a file
+    const filePath = buldFeedbackPath();
+    const data = extractFeedback(filePath);
+
+    data.push(newFeedback);
+    fs.writeFileSync(filePath, JSON.stringify(data));
+
+    return res.status(201).json({ message: "Success!", feedback: newFeedback });
+  }
+
+  const filePath = buldFeedbackPath();
+  const data = extractFeedback(filePath);
+
+  res.status(200).json({ feedback: data });
+}
+
+export default handler;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,65 @@
+import { useRef, useState } from "react";
+
 function HomePage() {
+  const [feedbackItems, setFeedbackItems] = useState([]);
+
+  const emailInputRef = useRef();
+  const feedbackInputRef = useRef();
+
+  function forSubmitHandler(event) {
+    event.preventDefault();
+
+    const email = emailInputRef.current.value;
+    const feedback = feedbackInputRef.current.value;
+
+    const reqBody = {
+      email: email,
+      feedback: feedback,
+    };
+
+    fetch("/api/feedback", {
+      method: "POST",
+      body: JSON.stringify(reqBody),
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }
+
+  function loadFeedbackHandler() {
+    fetch("/api/feedback")
+      .then((response) => response.json())
+      .then((data) => {
+        setFeedbackItems(data.feedback);
+      });
+  }
+
   return (
     <div>
       <h1>The Home Page</h1>
+      <form onSubmit={forSubmitHandler}>
+        <div>
+          <label htmlFor="email"></label>
+          <input type="email" name="email" id="email" ref={emailInputRef} />
+        </div>
+        <div>
+          <label htmlFor="feedback"></label>
+          <textarea
+            name="feedback"
+            id="feedback"
+            rows="5"
+            ref={feedbackInputRef}
+          ></textarea>
+        </div>
+        <button>Send feedback</button>
+      </form>
+      <hr />
+      <button onClick={loadFeedbackHandler}>Load feedback</button>
+      <ul>
+        {feedbackItems.map((item) => (
+          <li key={item.id}>{item.text}</li>
+        ))}
+      </ul>
     </div>
   );
 }
