e3839043a561ac6ce01c67dc8bfd8d1449cc6818
PiotrekNiemirski <piotr.niemirski@they.pl>
2022-11-13 21:36:58 +0100

    feat: user authentication

--- pages/auth.js
+++ pages/auth.js
@@ -1,6 +1,24 @@
-import AuthForm from '../components/auth/auth-form';
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import { getSession } from "next-auth/client";
+import AuthForm from "../components/auth/auth-form";
 
 function AuthPage() {
+  const [isLoading, setIsLoading] = useState(true);
+  const router = new useRouter();
+
+  useEffect(() => {
+    getSession().then((session) => {
+      if (session) {
+        router.replace("/");
+      } else {
+        setIsLoading(false);
+      }
+    });
+  }, []);
+
+  if (isLoading) return <p>Loading...</p>;
+
   return <AuthForm />;
 }
 
